33136ec85f971b55e84d31f2526c43f0a12e9f83
prief <207792089@qq.com>
2023-02-12 14:53:36 +0800

    init vue

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <img alt="Logo" class="logo" src="@/assets/logo.svg" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  padding: 10px;
+  align-items: center;
+}
+
+.logo {
+  width: 35px;
+  height: 35px;
+}
+
+nav {
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "../components/HelloWorld.vue";
+import { useCounterStore } from "@/stores/counter";
+const counter = useCounterStore();
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="Hi." />
+    <p>
+      You've clicked {{ counter.count }} times.
+      <button @click="counter.increment()">click</button>
+    </p>
+  </main>
+</template>
